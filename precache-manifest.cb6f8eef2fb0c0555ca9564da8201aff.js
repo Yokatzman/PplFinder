ed20ccd88cabadff67d0d88c492388b3954ee6c2
yoav <yoav.katzman@campus.technion.ac.il>
2021-12-20 15:14:36 +0200

    Updates

---
+++ precache-manifest.cb6f8eef2fb0c0555ca9564da8201aff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4b9f0a87f41541fad711579ecc1594",
+    "url": "/PplFinder/index.html"
+  },
+  {
+    "revision": "73329e988f0d03e06531",
+    "url": "/PplFinder/static/js/2.2f5e20ec.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/PplFinder/static/js/2.2f5e20ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06865b9ebdfc4d455c17",
+    "url": "/PplFinder/static/js/main.389199a2.chunk.js"
+  },
+  {
+    "revision": "32eb9f5c17181ba0ef19",
+    "url": "/PplFinder/static/js/runtime-main.b6c1fb24.js"
+  }
+]);
